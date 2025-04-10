f96362a59bcdd87c5a5d12e2e6e9e40f5935f141
João Emanuel <166662967+joaoemanuelrocha@users.noreply.github.com>
2025-04-10 19:28:25 +0000

    Desafio nível aventureiro concluído

---
+++ nivel_aventureiro.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+
+int main() {
+    char cadastro[25];
+    char login[25];
+    char estado;
+    char carta[20];
+    char cidade [30];
+    int populacao;
+    int senha;
+    int turísticos;
+    float área;
+    float pib;
+    float densidade;
+    float capita;
+    
+
+    printf("Carta 1: \n");
+    // Carta 1
+    printf("Faça seu cadastro: \n");
+    scanf("%s", &cadastro);
+    // Cadastro do usuário
+    printf("Coloque sua senha: \n");
+    scanf("%d", &senha);
+    // Senha do usuário
+    printf("Digite seu estado: \n");
+    scanf("%s", &estado);
+    // Estado do usuário
+    printf("Digite o código da carta: \n");
+    scanf("%s", &carta);
+    // Código da carta do usuário
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &cidade);
+    // Cidade do usuário
+    printf("Digite o número de habitantes: \n");
+    scanf("%d", &populacao);
+    // Número de habitantes da cidade do usuário
+    printf("Digite a área: \n");
+    scanf("%f", &área);
+    // Área da cidade do usuário
+    printf("Digite o PIB: \n");
+    scanf("%f", &pib);
+    // PIB da cidade do usuário
+    printf("Digite os pontos turísticos: \n");
+    scanf("%d", &turísticos);
+    // Locais turísticos da cidade do usuário
+    densidade = (float) (populacao / área);
+    printf("A densidade populacional é: %.3f hab/km²\n", densidade);
+    // Calculo para saber a densidade populacional
+    capita = (float) (pib / populacao);
+    printf("O PIB per capita é: %.3f reais\n", capita);
+    // Calculo para saber o PIB per capita
+
+
+    printf("Carta 2: \n");
+    // Carta 2
+    printf("Faça seu login: \n");
+    scanf("%s", &login);
+    // Login do usuário
+    printf("Coloque sua senha: \n");
+    scanf("%d", &senha);
+    // Senha do usuário
+    printf("Digite seu estado: \n");
+    scanf("%s", &estado);
+    // Estado do usuário
+    printf("Digite o código da carta: \n");
+    scanf("%s", &carta);
+    // Código da carta do usuário
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &cidade);
+    // Cidade do usuário
+    printf("Digite o número de habitantes: \n");
+    scanf("%d", &populacao);
+    // Número de habitantes da cidade do usuário
+    printf("Digite a área: \n");
+    scanf("%f", &área);
+    // Área da cidade do usuário
+    printf("Digite o PIB: \n");
+    scanf("%f", &pib);
+    // PIB da cidade do usuário
+    printf("Digite os pontos turísticos: \n");
+    scanf("%d", &turísticos);
+     // Locais turísticos da cidade do usuário
+     densidade = (float) (populacao / área);
+    printf("A densidade populacional é: %.3f hab/km²\n", densidade);
+    // Calculo para saber a densidade populacional
+    capita = (float) (pib / populacao);
+    printf("O PIB per capita é: %.3f reais\n", capita);
+    // Calculo para saber o PIB per capita
+
+    return 0;
+}
